8e5d4116d70b94251d094dc062814cdf3b54f30e
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-04-01 10:32:26 +0700

    feat: bloc arsitecture buat fetch dan event done

--- lib/bloc/weather_bloc_bloc.dart
+++ lib/bloc/weather_bloc_bloc.dart
@@ -1,13 +1,34 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weather/weather.dart';
+import 'package:weather_app/data/my_data.dart';
 
 part 'weather_bloc_event.dart';
 part 'weather_bloc_state.dart';
 
 class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
   WeatherBlocBloc() : super(WeatherBlocInitial()) {
-    on<WeatherBlocEvent>((event, emit) {
-      // TODO: implement event handler
+    on<FetchWeather>((event, emit) async{
+
+      emit(WeatherBlocLoading());
+
+      try {
+        WeatherFactory wf = WeatherFactory(WEATHER_API_KEY, language: Language.INDONESIAN);
+
+        // geolocation
+        Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+        // Cuaca Fetch
+        Weather weather = await wf.currentWeatherByLocation(
+          position.latitude, 
+          position.longitude
+        );
+
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
     });
   }
 }
